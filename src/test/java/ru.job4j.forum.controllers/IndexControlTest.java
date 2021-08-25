0f6a95af81fbd6fb335834675275c90e1c5419e6
zely20@mail.ru <Civik1984>
2021-08-25 17:28:01 +0300

    add test

---
+++ src/test/java/ru.job4j.forum.controllers/IndexControlTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.forum.controllers;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.Main;
+import ru.job4j.forum.service.PostService;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+public class IndexControlTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService topics;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnHomePage() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
