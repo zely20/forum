5254441ed402cb70e2328eccfc605c7e4f60af1d
zely20@mail.ru <Civik1984>
2021-08-26 20:39:24 +0300

    add create and update post test

--- src/main/java/ru/job4j/forum/controllers/PostControl.java
+++ src/main/java/ru/job4j/forum/controllers/PostControl.java
@@ -2,10 +2,7 @@ package ru.job4j.forum.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.forum.entity.Post;
 import ru.job4j.forum.service.PostService;
 
--- src/test/java/ru.job4j.forum.controllers/PostControlTest.java
+++ src/test/java/ru.job4j.forum.controllers/PostControlTest.java
@@ -1,17 +1,25 @@
 package ru.job4j.forum.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.entity.Post;
+import ru.job4j.forum.service.PostService;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -19,6 +27,9 @@ public class PostControlTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private PostService posts;
 
     @Test
     @WithMockUser
@@ -36,5 +47,32 @@ public class PostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    public void createPost() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("name","Куплю ладу-грант.")
+                .param("description", "Дорого."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант."));
+    }
+
+    @Test
+    @WithMockUser
+    public void UpdatePost() throws Exception {
+        this.mockMvc.perform(post("/edit")
+                .param("name","Update test post")
+                .param("description", "Дорого."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/index"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).update(argument.capture());
+        assertThat(argument.getValue().getName(), is("Update test post"));
     }
 }
